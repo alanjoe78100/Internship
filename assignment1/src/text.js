a978b6c4db2950f02c2f80321c6c6a39c0b12b9a
alanjoe78100 <alanjoe2003@gmail.com>
2024-09-25 21:59:44 +0530

    first commit

---
+++ assignment1/src/text.js
@@ -0,0 +1,26 @@
+// const a = 99;
+// let b = 77;
+// const c = a + b;
+// console.log(c);
+
+// let age = 1; let Age = 2; age == Age
+// console.log(age);
+
+
+function win (name) {
+    const as =  name; 
+    console.log(as) ;
+
+}
+
+win('ravi');
+
+// function win1(){
+//     console.log("You win!");
+
+// }
+
+// win1();
+
+
+
